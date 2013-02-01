fe4ba75854eed95a5bb89f238b672f64274f2d26
Prayag <prayag.upd@gmail.com>
2013-02-01 16:33:17 +0545

    ADD hiccup dependency for HTML templating

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject hotel "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "nepleaks"
+  :url "http://nepleaks.org/"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
+		 [hiccup "1.0.0"]
                  [mysql/mysql-connector-java "5.1.16"]
                 ]
   :plugins [[lein-ring "0.7.1"]]
