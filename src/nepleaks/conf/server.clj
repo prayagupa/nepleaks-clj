bc7471a0befdab5924b2d309f633983fba89f63b
iPrayag <prayag.upd@gmail.com>
2014-01-31 17:55:35 +0545

    REFACTOR 'main' method to 'getJson' in server.clj

--- src/nepleaks/conf/server.clj
+++ src/nepleaks/conf/server.clj
@@ -1,14 +1,13 @@
+;;
 (ns nepleaks.conf.server
   (:require [clojurewerkz.elastisch.rest :as esr]
             [clojurewerkz.elastisch.rest.document :as esd]
-            [clojurewerkz.elastisch.response :as esrsp]
   ))
 
-(defn -main
-  [& args]
-  (esr/connect! "http://127.0.0.1:9200"))
+(defn getJson []
+  (esr/connect! "http://127.0.0.1:9200")
 
   ;; fetch a single document by a known id
-  (esd/get "1000" "MemberSearch" "g:0xad8c81f57a9c06b293a87d54cb458126"))
-
+  (esd/get "1000" "MemberSearch" "g:0xad8c81f57a9c06b293a87d54cb458126")
+)
 
