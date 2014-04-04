f02311cf7f079c86a022173b869299a100687d39
iPrayag <prayag.upd@gmail.com>
2014-04-04 14:39:44 +0545

    ADD opennlp models

---
+++ models/models.sh
@@ -0,0 +1,12 @@
+#
+# @see : http://opennlp.sourceforge.net/models-1.5/
+#
+wget http://opennlp.sourceforge.net/models-1.5/en-sent.bin
+sleep 1
+wget http://opennlp.sourceforge.net/models-1.5/en-token.bin
+sleep 1
+wget http://opennlp.sourceforge.net/models-1.5/en-pos-maxent.bin
+
+echo "##########################################################"
+echo "################# models download complete ###############"
+echo "##########################################################"
