b2d47dac90417187826a6af98e020c09eb777b05
Prayag Upd <prayag.upd@gmail.com>
2014-04-23 02:35:30 +0545

    FIXES #4 to insert nodes with relation

--- nepleaks-engine/src/nepleaks_engine/services/neo4jService.clj
+++ nepleaks-engine/src/nepleaks_engine/services/neo4jService.clj
@@ -5,9 +5,9 @@
 
 (ns nepleaks-engine.services.neo4jService
   (:require [clojurewerkz.neocons.rest               :as nr]
-            [clojurewerkz.neocons.rest.nodes         :as nn])
-            ;;[clojurewerkz.neocons.rest.relationships :as nrl]
-)
+            [clojurewerkz.neocons.rest.nodes         :as nn]
+            [clojurewerkz.neocons.rest.relationships :as nrl]
+            [clojurewerkz.neocons.rest.cypher        :as cy]))
 
 ;; connects to the default Neo4J Server host/port/path
 (defn connectToNeoleaks []
@@ -29,17 +29,17 @@
                    ;; printing here will force the lazy response sequence to be evaluated
                    (println nodes)))
 
-(comment
+
 (defn insertNodesWithRelation []
  (let [govt    (nn/create {:title "Govt files"                 :leaker "prayagupd"})
        embassy (nn/create {:title "Embassy Diplomatics cables" :leaker "j"})
        rel     (nrl/create govt embassy :corruption {:on "2011"})]
     (println (nn/get (:id govt)))
-    (println (nn/get (:id embassy))))))
+    (println (nn/get (:id embassy)))))
 
 ;;
 ;;@see : http://clojureneo4j.info/articles/populating.html
 ;;
 (defn bootstrapNeoleaks []
   (connectToNeoleaks)
-  (insertNodesInBatch))
+  (insertNodesWithRelation))
