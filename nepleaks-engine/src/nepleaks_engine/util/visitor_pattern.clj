665195c0f42cd08b27a1fc30d1e6bfcca3fcc288
prayagupd <prayag.upd@gmail.com>
2015-04-14 23:31:45 -0500

    refactor visiter pattern

--- nepleaks-engine/src/nepleaks_engine/util/visitor_pattern.clj
+++ nepleaks-engine/src/nepleaks_engine/util/visitor_pattern.clj
@@ -1,24 +1,34 @@
 
+;;
+;; author prayagupd
+;; references
+;; http://clojure.org/runtime_polymorphism
+;; https://clojuredocs.org/clojure.core/defmulti
+;; https://weblogs.java.net/blog/manningpubs/archive/2013/02/20/multimethods-clojure
+;;
+
 (ns nepleaks-engine.util.visitor-pattern)
 
 ;;(defmulti name dispatch-fn & options)
 ;;(defmethod multifn dispatch-value & fn-tail)
 
 ;; EXPECTED RESULT
-;; For user1  30.0
-;; For user2  9.0
-;; For user3  14.0
+;; For :mint.com   (user1)  30.0
+;; For :google.com (user2)  9.0
+;; For :yahoo.com  (user3)  14.0
+
+
+(defn compute-amount [percentage user]
+  (float (* 0.01 percentage (:salary user))))
 
 
 ;;
+;; abstraction on referrer
 ;; public interface Visitable {
 ;;   public <T> void accept(Visitor<T> v);
 ;; }
 
 (defmulti accept :referrer)
-
-(defn compute-amount [percentage user]
-  (float (* 0.01 percentage (:salary user))))
 
 (defmethod accept :mint.com [user]
   (compute-amount 0.03 user))
@@ -29,11 +39,16 @@
 (defmethod accept :default [user]
   (compute-amount 0.02 user))
 
+
+;;
+;; uses
+;;
+
 (def user-1 {:login "prayag upd"    :referrer :mint.com   :salary 100000})
 (def user-2 {:login "rajesh hamal"  :referrer :google.com :salary 90000})
 (def user-3 {:login "steven wilson" :referrer :yahoo.com  :salary 70000})
 
 (defn visitor []
- (println "For user1 " (accept user-1))
- (println "For user2 " (accept user-2))
- (println "For user3 " (accept user-3)))
+ (println "For :mint.com   (user1) " (accept user-1))
+ (println "For :google.com (user2) " (accept user-2))
+ (println "For :yahoo.com  (user3) " (accept user-3)))
